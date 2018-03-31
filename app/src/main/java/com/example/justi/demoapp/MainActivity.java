4f9592c3a073408e5e9247503b116853aede2ab1
Justin Tappert <justin@theflybook.com>
2018-03-31 13:38:23 -0700

    Little sample beat app complete

--- app/src/main/java/com/example/justi/demoapp/MainActivity.java
+++ app/src/main/java/com/example/justi/demoapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.justi.demoapp;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,7 +14,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void clickFunction(View view) {
-        Log.i("Info", "Button pressed!");
+    public void buttonTapped(View view) {
+        int id = view.getId();
+        String ourId = "";
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.example.justi.demoapp");
+
+        MediaPlayer player = MediaPlayer.create(this, resourceId);
+        player.start();
+
+        Log.i("button tapped", ourId);
     }
 }
